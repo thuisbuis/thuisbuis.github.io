2fd79b5d8fc4cd72129cf8bf696c4d702f5ffe8e
Danny Engelman <danny@engelman.nl>
2021-07-30 13:26:50 +0200

    Update dynamic video/lang

--- elements.videomeister.js
+++ elements.videomeister.js
@@ -1,52 +1,55 @@
-!((// in IIFE so nothing in the global scope, no export either
-) => {
+!(() =>
+  // in IIFE so nothing in the global scope, no export either
+  {
     let $ = (x, root = document) => root.getElementById(x);
-    let createElement = (
-        tag,
-        content = {},
-        inject = false
-    ) => (
-            tag = document.createElement(tag, { is: content.is }),
-            typeof content === 'string'
-                ? tag.innerHTML = content
-                //todo add append
-                : Object.keys(content)
-                    .map(attr => {
-                        if (tag.hasOwnProperty(attr)) {
-                            tag[attr] = content[attr]
-                        } else
-                            tag.setAttribute(attr, content[attr])
-                    }),
-            //console.log(tag)            ,
-            inject
-                ? inject.append(tag)
-                : null,
-            tag// return
-        );
+    let createElement = (tag, content = {}, inject = false) => (
+      (tag = document.createElement(tag, { is: content.is })),
+      typeof content === "string"
+        ? (tag.innerHTML = content)
+        : //todo add append
+          Object.keys(content).map((attr) => {
+            if (tag.hasOwnProperty(attr)) {
+              tag[attr] = content[attr];
+            } else tag.setAttribute(attr, content[attr]);
+          }),
+      //console.log(tag)            ,
+      inject ? inject.append(tag) : null,
+      tag // return
+    );
 
     function formatSecondsAsTime(secs, msec, format) {
-        var hr = Math.floor(secs / 3600);
-        var min = Math.floor((secs - (hr * 3600)) / 60);
-        var sec = Math.floor(secs - (hr * 3600) - (min * 60));
-        var msec_accuracy = 1000;
-        msec = (msec) ? '.' + Math.floor(((secs % 1000) - sec) * msec_accuracy) : ''; //empty if second parameter msec==false
+      var hr = Math.floor(secs / 3600);
+      var min = Math.floor((secs - hr * 3600) / 60);
+      var sec = Math.floor(secs - hr * 3600 - min * 60);
+      var msec_accuracy = 1000;
+      msec = msec
+        ? "." + Math.floor(((secs % 1000) - sec) * msec_accuracy)
+        : ""; //empty if second parameter msec==false
 
-        if (hr < 10) { hr = "0" + hr; }
-        if (min < 10) { min = "0" + min; }
-        if (sec < 10) { sec = "0" + sec; }
-        if (hr) { hr = "00"; }
+      if (hr < 10) {
+        hr = "0" + hr;
+      }
+      if (min < 10) {
+        min = "0" + min;
+      }
+      if (sec < 10) {
+        sec = "0" + sec;
+      }
+      if (hr) {
+        hr = "00";
+      }
 
-        if (format != null) {
-            var formatted_time = format.replace('hh', hr);
-            formatted_time = formatted_time.replace('h', hr * 1 + ""); // check for single hour formatting
-            formatted_time = formatted_time.replace('mm', min);
-            formatted_time = formatted_time.replace('m', min * 1 + ""); // check for single minute formatting
-            formatted_time = formatted_time.replace('ss', sec);
-            formatted_time = formatted_time.replace('s', sec * 1 + ""); // check for single second formatting
-            return formatted_time;
-        } else {
-            return hr + ':' + min + ':' + sec + msec;
-        }
+      if (format != null) {
+        var formatted_time = format.replace("hh", hr);
+        formatted_time = formatted_time.replace("h", hr * 1 + ""); // check for single hour formatting
+        formatted_time = formatted_time.replace("mm", min);
+        formatted_time = formatted_time.replace("m", min * 1 + ""); // check for single minute formatting
+        formatted_time = formatted_time.replace("ss", sec);
+        formatted_time = formatted_time.replace("s", sec * 1 + ""); // check for single second formatting
+        return formatted_time;
+      } else {
+        return hr + ":" + min + ":" + sec + msec;
+      }
     }
     /*
         load
@@ -58,7 +61,7 @@
         play				Fired whenever the media begins or resumes playback.
         pause				Fired whenever the media has been paused.
         timeupdate			Fired when the current playback position has changed. During playback this is fired every
-        15-250 milliseconds, depnding on the playback technology in use.
+        15-250 milliseconds, depending on the playback technology in use.
         ended				Fired when the end of the media resource is reached. currentTime == duration
         durationchange		Fired when the duration of the media resource is changed, or known for the first time.
         progress			Fired while the user agent is downloading media data.
@@ -69,175 +72,211 @@
      */
 
     let attachListeners = ({
-        root = window,
-        log = function (evt) {
-            let args = [...arguments];
-            args.shift();
-            console.log(`%c RT Video: ${evt.type}`, 'background:gold', ...args)
-        },
-        name = { [window]: 'window', [document]: 'document' }[root] || root.nodeName,
-        events = [
-            // ["load"],
-            // ["DOMContentLoaded"],
-            // ["readystatechange"],
-        ]
-    }) => events.map(
-        ([
-            eventName,
-            eventFunc = log
-        ]) => {
-            let func = evt => {
-                log(evt);
-                return eventFunc(evt);
-            }
-            root.addEventListener(eventName, func);
-            return () => root.removeEventListener(eventName, func)
-        })
+      root = window,
+      log = function (evt) {
+        let args = [...arguments];
+        args.shift();
+        console.log(`%c RT Video: ${evt.type}`, "background:gold", ...args);
+      },
+      name = { [window]: "window", [document]: "document" }[root] ||
+        root.nodeName,
+      events = [
+        // ["load"],
+        // ["DOMContentLoaded"],
+        // ["readystatechange"],
+      ],
+    }) =>
+      events.map(([eventName, eventFunc = log]) => {
+        let func = (evt) => {
+          log(evt);
+          return eventFunc(evt);
+        };
+        root.addEventListener(eventName, func);
+        return () => root.removeEventListener(eventName, func);
+      });
 
-    customElements.define('roads-video', class extends HTMLElement {
+    customElements.define(
+      "roads-video",
+      class extends HTMLElement {
         constructor() {
-            super()
-            this.removeListeners = [
-                ...attachListeners(window),
-                ...attachListeners(document),
-                ...attachListeners(this),
-            ];
-            var template = document.getElementById('roads-video-template1').content;
-            this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true));
-            this.currentChapter = 1;
-            this.lastChapter = 0;
-            this.initElement = true;// canplay event fires also on seek!
+          super();
+          this.removeListeners = [
+            ...attachListeners(window),
+            ...attachListeners(document),
+            ...attachListeners(this),
+          ];
+          let params = new URLSearchParams(location.search);
+          let html = document
+            .getElementById(this.nodeName)
+        .innerHTML
+            .replaceAll("[VIDEO]", params.get("video")||"gebarsten_emmer")
+            .replaceAll("[LANG]", params.get("lang")||"nl");
+          // read HTML template
+          // get URL parameter [video]
+          // get URL parameter [lang]
+          // create innerHTML
+
+          this.attachShadow({ mode: "open" }).innerHTML = html;
+          this.currentChapter = 1;
+          this.lastChapter = 0;
+          this.initElement = true; // canplay event fires also on seek!
         }
         $(selector) {
-            return this.shadowRoot.querySelector(selector)
+          return this.shadowRoot.querySelector(selector);
         }
         title(txt) {
-            this.$('#title').innerHTML = txt + ' ' + this.getAttribute("title");
+          this.$("#title").innerHTML = txt + " " + this.getAttribute("title");
         }
         play() {
-            this.classList.add("playing");
-            this.playbutton.classList.add("playing");
-            this.title('Playing');
-            this.video.play();
+          this.classList.add("playing");
+          this.playbutton.classList.add("playing");
+          this.title("Playing");
+          this.video.play();
         }
         pause() {
-            this.classList.remove("playing");
-            this.playbutton.classList.remove("playing");
-            this.title('Paused');
-            this.video.pause();
+          this.classList.remove("playing");
+          this.playbutton.classList.remove("playing");
+          this.title("Paused");
+          this.video.pause();
         }
         get paused() {
-            return this.playbutton.classList.contains("playing")
+          return this.playbutton.classList.contains("playing");
         }
         toggle() {
-            if (!this.paused) this.play();
-            else this.pause();
+          if (!this.paused) this.play();
+          else this.pause();
         }
         seek(chapter) {
-            //this.currentChapter = 1;
-            this.chapters.map(chapter => chapter.div.seen(false));
-            this.video.currentTime = chapter.getAttribute("time");
-            this.play();
-            this.play();
+          //this.currentChapter = 1;
+          this.chapters.map((chapter) => chapter.div.seen(false));
+          this.video.currentTime = chapter.getAttribute("time");
+          this.play();
+          this.play();
         }
         connectedCallback() {
-            window.setTimeout(() => {
-                let $element = this;
-                this.video = this.$("video");
-                this.playbutton = this.$(".play-button");
-                this.title("Ready");
-                attachListeners({
-                    root: this.video,
-                    events: [
-                        ["loadstart"],
-                        ["loadedmetadata"],
-                        ["loadeddata"],
-                        ["play"],
-                        ["pause"],
-                        ["timeupdate", function (evt) {
-                            let timestamp = $element.video.currentTime;
-                            $element.$(".time").innerHTML = formatSecondsAsTime(timestamp, true);
-                            let chapter, nr = 0;
-                            chapter = $element.chapters[nr];
-                            while (chapter && timestamp > chapter.time) {
-                                chapter.div.seen(true);
-                                //console.log(timestamp > chapter.time, $element.currentChapter, $element.lastChapter)
-                                chapter = $element.chapters[++nr];
-                            }
-                            $element.currentChapter = nr;
-                            $element.setAttribute("chapter", $element.currentChapter);
-                        }],
-                        ["canplay", function (evt) {
-                            //$element.pause();
-                            //$element.pause();
-                            //this = <video>
-                            if ($element.initElement) {
-                                $element.video.addEventListener("click", evt => $element.toggle());
-                                $element.playbutton.addEventListener("click", evt => $element.toggle());
-                                $element.setAttribute("chapter", $element.currentChapter);
-                                $element.pause();
-                            }
-                            $element.initElement = false;
-                        }],
-                        ["ended", function (evt) {
-                            //$element.pause();
-                            $element.title('Ended');
-                        }]
-                    ]
-                })
-                this.chapters = this.innerHTML
-                    .split("#")
-                    .map(line => line
-                        .trim()
-                        .split("=="))
-                    .filter(x => x[1])
-                    .map(([time, title], idx) => {
-                        let div = createElement("roads-video-chapter");
-                        div.title = title;
-                        div.setAttribute("time", time);
-                        div.setAttribute("chapter", idx + 1);
-                        this.lastChapter++;
-                        return {
-                            chapterNr: idx + 1,
-                            time: parseFloat(time),
-                            title: title.trim(),
-                            div
-                        }
-                    });
-
-                this.$(".chapters").innerHTML = '';
-                this.$(".chapters").append(...this.chapters.map(chapter => chapter.div));
+          window.setTimeout(() => {
+            let $element = this;
+            this.video = this.$("video");
+            this.playbutton = this.$(".play-button");
+            this.title("Ready");
+            attachListeners({
+              root: this.video,
+              events: [
+                ["loadstart"],
+                ["loadedmetadata"],
+                ["loadeddata"],
+                ["play"],
+                ["pause"],
+                [
+                  "timeupdate",
+                  function (evt) {
+                    let timestamp = $element.video.currentTime;
+                    $element.$(".time").innerHTML = formatSecondsAsTime(
+                      timestamp,
+                      true
+                    );
+                    let chapter,
+                      nr = 0;
+                    chapter = $element.chapters[nr];
+                    while (chapter && timestamp > chapter.time) {
+                      chapter.div.seen(true);
+                      //console.log(timestamp > chapter.time, $element.currentChapter, $element.lastChapter)
+                      chapter = $element.chapters[++nr];
+                    }
+                    $element.currentChapter = nr;
+                    $element.setAttribute("chapter", $element.currentChapter);
+                  },
+                ],
+                [
+                  "canplay",
+                  function (evt) {
+                    //$element.pause();
+                    //$element.pause();
+                    //this = <video>
+                    if ($element.initElement) {
+                      $element.video.addEventListener("click", (evt) =>
+                        $element.toggle()
+                      );
+                      $element.playbutton.addEventListener("click", (evt) =>
+                        $element.toggle()
+                      );
+                      $element.setAttribute("chapter", $element.currentChapter);
+                      $element.pause();
+                    }
+                    $element.initElement = false;
+                  },
+                ],
+                [
+                  "ended",
+                  function (evt) {
+                    //$element.pause();
+                    $element.title("Ended");
+                  },
+                ],
+              ],
             });
+            this.chapters = this.innerHTML
+              .split("#")
+              .map((line) => line.trim().split("=="))
+              .filter((x) => x[1])
+              .map(([time, title], idx) => {
+                let div = createElement("roads-video-chapter");
+                div.title = title;
+                div.setAttribute("time", time);
+                div.setAttribute("chapter", idx + 1);
+                this.lastChapter++;
+                return {
+                  chapterNr: idx + 1,
+                  time: parseFloat(time),
+                  title: title.trim(),
+                  div,
+                };
+              });
+
+            this.$(".chapters").innerHTML = "";
+            this.$(".chapters").append(
+              ...this.chapters.map((chapter) => chapter.div)
+            );
+          });
         }
         disconnectedCallback() {
-            this.removeListeners(removeFunc => removeFunc());
+          this.removeListeners((removeFunc) => removeFunc());
         }
-    });
+      }
+    );
 
-    customElements.define("roads-video-chapter", class extends HTMLElement {
+    customElements.define(
+      "roads-video-chapter",
+      class extends HTMLElement {
         constructor() {
-            super();
+          super();
         }
         seen(state = false) {
-            this.classList.toggle("chapter--seen", state);
+          this.classList.toggle("chapter--seen", state);
         }
         connectedCallback() {
-            setTimeout(() => {
-                let $roads_video = this.getRootNode().host;
-                let time = this.getAttribute("time");
-                let title = this.getAttribute("title");
-                let chapterNr = this.getAttribute("chapter");
-                let timespan = createElement('SPAN', String(formatSecondsAsTime(time)));
-                timespan.classList.add("chapter--time");
-                let titlespan = createElement('SPAN', title);
-                titlespan.classList.add("chapter--title")
-                this.id = "chapter" + chapterNr;
-                this.append(timespan, titlespan);
-                attachListeners({
-                    root: this,
-                    events: [["click", evt => $roads_video.seek(this)]]
-                })
-            })
+          setTimeout(() => {
+            let $roads_video = this.getRootNode().host;
+            let time = this.getAttribute("time");
+            let title = this.getAttribute("title");
+            let chapterNr = this.getAttribute("chapter");
+            let timespan = createElement(
+              "SPAN",
+              String(formatSecondsAsTime(time))
+            );
+            timespan.classList.add("chapter--time");
+            let titlespan = createElement("SPAN", title);
+            titlespan.classList.add("chapter--title");
+            this.id = "chapter" + chapterNr;
+            this.append(timespan, titlespan);
+            attachListeners({
+              root: this,
+              events: [["click", (evt) => $roads_video.seek(this)]],
+            });
+          });
         }
-    });
-})({/* inject default icons here */ });
+      }
+    );
+  })({
+  /* inject default icons here */
+});
